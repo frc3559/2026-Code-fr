6f9693c427cac7aa9ebd036b4efeae36e24e1647
Alex <13315@student.gjcs.k12.in.us>
2026-02-21 11:59:23 -0500

    Soft Limits for hook and intake pivot

--- src/main/java/frc/robot/subsystems/HookSubsystem.java
+++ src/main/java/frc/robot/subsystems/HookSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SoftLimitConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -11,8 +15,13 @@ public class HookSubsystem extends SubsystemBase {
 
     public HookSubsystem() {
         hook = new SparkMax(kHookMotorCanId, MotorType.kBrushless);
-
-    }
+        SoftLimitConfig hookLimitConfig = new SoftLimitConfig(); //The soft limit code should stop the motor when it hits one of the specified values.
+        hookLimitConfig.forwardSoftLimit(100); //We do not know whether the limit value is degrees or rotations
+        hookLimitConfig.reverseSoftLimit(0); //These lines make no forward and reverse limits. tweak values based on testing.
+        SparkMaxConfig hookConfig = new SparkMaxConfig();
+        hookConfig.apply(hookLimitConfig); //applies the hooklimitconfig's limits to the spark max config called hookconfig
+        hook.configure(hookConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters); //Change persist and reset paramaters to no or blank based on what we want
+    } //the above line configures the motor hook based on hookConfig
 
     public void hookOn(double speed) {
         hook.set(speed);
--- src/main/java/frc/robot/subsystems/IntakePivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakePivotSubsystem.java
@@ -2,6 +2,13 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+//import com.revrobotics.spark.config.SparkBaseConfig; unnecessary
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SoftLimitConfig;
+//import com.revrobotics.spark.config.SoftLimitConfigAccessor; unnecessary
+import com.revrobotics.ResetMode;
+import com.revrobotics.PersistMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -11,7 +18,13 @@ public class IntakePivotSubsystem extends SubsystemBase {
 
     public IntakePivotSubsystem() {
         intakePivot = new SparkMax(kIntakePivotMotorCanId, MotorType.kBrushless);
-    }
+        SoftLimitConfig pivotLimitConfig = new SoftLimitConfig(); //The soft limit code should stop the motor when it hits one of the specified values.
+        pivotLimitConfig.forwardSoftLimit(100); //We do not know whether the limit value is degrees or rotations
+        pivotLimitConfig.reverseSoftLimit(0); //These lines make no forward and reverse limits. tweak values based on testing.
+        SparkMaxConfig pivotConfig = new SparkMaxConfig();
+        pivotConfig.apply(pivotLimitConfig); //applies the pivotlimitconfig's limits to the spark max config called pivotconfig
+        intakePivot.configure(pivotConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters); //Change persist and reset paramaters to no or blank based on what we want
+    } //the above line configures the motor intakePivot based on pivotConfig
 
     public void intakePivotUp(double speed) {
         intakePivot.set(speed);
